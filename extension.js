25a71df6be2f665149611f0442c0ebf92774dd34
Morre <morre@mor.re>
2019-09-27 10:52:53 +0200

    Add default message, remove unused code

--- extension.js
+++ extension.js
@@ -2,33 +2,10 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Gio = imports.gi.Gio;
 
-let text, button;
-
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+let button;
 
 function init() {
     button = new St.Bin({ style_class: 'panel-button',
@@ -37,10 +14,10 @@ function init() {
                           x_fill: true,
                           y_fill: false,
                           track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
 
-    button.set_child(icon);
+    let label = new St.Label({text: "[ICE Portal] Unreachable"})
+
+    button.set_child(label);
     button.connect('button-press-event', _showHello);
 }
 
