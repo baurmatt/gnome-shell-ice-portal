5bee4f46ffb447545fc2b30f773c722acb541426
Morre <morre@mor.re>
2019-09-27 12:10:05 +0200

    Add basic functionality to display speed

--- extension.js
+++ extension.js
@@ -1,30 +1,92 @@
-
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
-const Me = imports.misc.extensionUtils.getCurrentExtension();
-const Gio = imports.gi.Gio;
+const Soup = imports.gi.Soup;
+const Lang = imports.lang;
+const Mainloop = imports.mainloop;
+const Clutter = imports.gi.Clutter;
+const PanelMenu = imports.ui.panelMenu;
 
-let button;
+const API_STATUS = 'https://iceportal.de/api1/rs/status';
+
+let _httpSession;
+const ICEPortalIndicator = new Lang.Class({
+  Name: 'ICEPortal',
+  Extends: PanelMenu.Button,
+
+  _init: function () {
+    this.parent(0.0, "ICE Portal indicator", false);
+    this.buttonText = new St.Label({
+      text: _("[ICE Portal connecting…]"),
+      y_align: Clutter.ActorAlign.CENTER
+    });
+    this.actor.add_actor(this.buttonText);
+    this._refresh();
+  },
+
+  _refresh: function () {
+    this._loadData(this._refreshUI);
+    this._removeTimeout();
+    this._timeout = Mainloop.timeout_add_seconds(10, Lang.bind(this, this._refresh));
+    return true;
+  },
+
+  _loadData: function () {
+    //_httpSession = new Soup.Session({"user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"});
+    _httpSession = new Soup.Session({"user-agent": "DB ICE Portal GNOME Shell extension"});
+
+    let message = Soup.form_request_new_from_hash('GET', API_STATUS, {});
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+          if (message.status_code != 200) {
+            global.log("ICE Portal " + message.status_code + " " + message.response_body.data);
+            return;
+          }
+
+          let json = JSON.parse(message.response_body.data);
+
+          // If there is an actual GPS signal, the gpsStatus is "VALID"
+          if (json['gpsStatus'] == "VALID") {
+            this._refreshUI(json['speed'].toString() + " km/h");
+          }
+        }
+      )
+    );
+  },
+
+  _refreshUI: function (txt) {
+    this.buttonText.set_text(txt);
+  },
+
+  _removeTimeout: function () {
+    if (this._timeout) {
+      Mainloop.source_remove(this._timeout);
+      this._timeout = null;
+    }
+  },
+
+  stop: function () {
+    if (_httpSession !== undefined)
+      _httpSession.abort();
+    _httpSession = undefined;
+
+    if (this._timeout)
+      Mainloop.source_remove(this._timeout);
+    this._timeout = undefined;
+
+    this.menu.removeAll();
+  }
+});
+
+let iceMenu;
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-
-    let label = new St.Label({text: "[ICE Portal] Unreachable"})
-
-    button.set_child(label);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+	iceMenu = new ICEPortalIndicator;
+	Main.panel.addToStatusArea('iceportal-indicator', iceMenu);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+	iceMenu.stop();
+	iceMenu.destroy();
 }
